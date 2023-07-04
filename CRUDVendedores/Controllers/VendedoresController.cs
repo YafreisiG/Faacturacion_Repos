fe86778ccdb78193f7f233b5a0e6f7c1b49f40f3
Yafreisi Geraldo Rosario <y.geraldo@unapec.edu.do>
2023-07-04 06:59:10 -0400

    subiendo los primeros archivos

---
+++ CRUDVendedores/Controllers/VendedoresController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CRUDVendedores.Models;
+
+namespace CRUDVendedores.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        private readonly VendedoresMvcContext _context;
+
+        public VendedoresController(VendedoresMvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vendedores
+        public async Task<IActionResult> Index()
+        {
+              return _context.Vendedores != null ? 
+                          View(await _context.Vendedores.ToListAsync()) :
+                          Problem("Entity set 'VendedoresMvcContext.Vendedores'  is null.");
+        }
+
+        // GET: Vendedores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Vendedores == null)
+            {
+                return NotFound();
+            }
+
+            var vendedore = await _context.Vendedores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendedore == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendedore);
+        }
+
+        // GET: Vendedores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vendedores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,PorcentajeComision,Estado")] Vendedore vendedore)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vendedore);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendedore);
+        }
+
+        // GET: Vendedores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Vendedores == null)
+            {
+                return NotFound();
+            }
+
+            var vendedore = await _context.Vendedores.FindAsync(id);
+            if (vendedore == null)
+            {
+                return NotFound();
+            }
+            return View(vendedore);
+        }
+
+        // POST: Vendedores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,PorcentajeComision,Estado")] Vendedore vendedore)
+        {
+            if (id != vendedore.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vendedore);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VendedoreExists(vendedore.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendedore);
+        }
+
+        // GET: Vendedores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Vendedores == null)
+            {
+                return NotFound();
+            }
+
+            var vendedore = await _context.Vendedores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendedore == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendedore);
+        }
+
+        // POST: Vendedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Vendedores == null)
+            {
+                return Problem("Entity set 'VendedoresMvcContext.Vendedores'  is null.");
+            }
+            var vendedore = await _context.Vendedores.FindAsync(id);
+            if (vendedore != null)
+            {
+                _context.Vendedores.Remove(vendedore);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VendedoreExists(int id)
+        {
+          return (_context.Vendedores?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CRUDVendedores/Models/Vendedore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDVendedores.Models;
+
+public partial class Vendedore
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public decimal? PorcentajeComision { get; set; }
+
+    public string? Estado { get; set; }
+}
